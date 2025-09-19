a58db1294405cd436cd7fb7f254bfeb5218ed7e6
Michael Gleason <mjgleaso@gmail.com>
2025-09-18 22:46:59 -0600

    adding tests for score-attributes command

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -109,5 +109,96 @@ def test_characterize_invalid_config(
     assert expected_contents in log, "Expected error message not found in log file"
 
 
+def test_score_attributes(
+    cli_runner,
+    tmp_path,
+    data_dir,
+):
+    """
+    Happy path test for the score-attributes command. Tests that it produces the
+    expected outputs for known inputs.
+    """
+    in_config_path = data_dir / "score_attributes" / "config.json"
+    with open(in_config_path, "r") as f:
+        config_data = json.load(f)
+    config_data["grid"] = (
+        data_dir / "characterize" / "outputs" / "grid_char.gpkg"
+    ).as_posix()
+
+    config_path = tmp_path / "config.json"
+    with open(config_path, "w") as f:
+        json.dump(config_data, f)
+
+    result = cli_runner.invoke(
+        main,
+        ["score-attributes", "-c", config_path.as_posix()],
+    )
+    assert result.exit_code == 0, f"Command failed with error {result.exception}"
+
+    out_gpkg = tmp_path / "grid_char_attr_scores.gpkg"
+    assert out_gpkg.exists(), "Output grid not created."
+
+    out_df = gpd.read_file(out_gpkg)
+
+    expected_gpkg = (
+        data_dir / "score_attributes" / "outputs" / "grid_char_attr_scores.gpkg"
+    )
+    expected_df = gpd.read_file(expected_gpkg)
+
+    assert_geodataframe_equal(expected_df, out_df)
+
+    logs = list((tmp_path / "logs").glob("*_score_attributes.log"))
+    assert len(logs) > 0, "No logs were created"
+
+    log = logs[0]
+    with open(log, "r") as f:
+        log_content = f.read()
+
+    assert (
+        "UserWarning: NAs encountered in results dataframe" in log_content
+    ), "Expected warning messages were not found in log file."
+    assert (
+        "Running scoring for output column" in log_content
+    ), "Expected progress messages were not found in log file."
+
+
+def test_score_attributes_invalid_config(
+    cli_runner,
+    tmp_path,
+    data_dir,
+):
+    """
+    Check for sane error message in log when an invalid configuration is passed.
+    """
+    in_config_path = data_dir / "score_attributes" / "config.json"
+    with open(in_config_path, "r") as f:
+        config_data = json.load(f)
+    config_data["grid"] = (
+        data_dir / "characterize" / "outputs" / "not-a-grid.gpkg"
+    ).as_posix()
+
+    config_path = tmp_path / "config.json"
+    with open(config_path, "w") as f:
+        json.dump(config_data, f)
+
+    result = cli_runner.invoke(
+        main,
+        ["score-attributes", "-c", config_path.as_posix()],
+    )
+    assert result.exit_code == 1
+
+    log_paths = list((tmp_path / "logs").glob("*.log"))
+    if len(log_paths) == 0:
+        raise ValueError("Logs were not created by command.")
+    log_path = log_paths[0]
+    with open(log_path, "r") as f:
+        log = f.read()
+    expected_contents = (
+        "Configuration did not pass validation. The following issues were identified:\n"
+        "1 validation error for ScoreAttributesConfig"
+    )
+    assert expected_contents in log, "Expected error message not found in log file"
+
+
 if __name__ == "__main__":
     pytest.main([__file__, "-s"])
